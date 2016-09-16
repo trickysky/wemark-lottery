1ae370a88af72c5480117397cd98fad29a17b47e
tk <iam@tiankun.me>
2016-09-17 00:08:46 +0800

    增加渲染位置

--- public/js/main.js
+++ public/js/main.js
@@ -1,150 +1,146 @@
 /**
  * Created by tk on 16/9/6.
  */
+$(function () {
+    var token = $('#token').text(),
+        id = $('#id').text(),
+        _csrf = $('#_csrf').text(),
+        longitude ,
+        latitude,
+        received = 'true' == $('#received').text(),
+        drawn = 'true' == $('#drawn').text(),
+        type = $('#type').text(),
+        prize_msg = $('#msg').text();
 
-var token = 'token',
-    id = 'id',
-    _csrf = '_csrf',
-    longitude = 'longitude',
-    latitude = 'latitude',
-    device = 'device',
-    client = 'client',
-    mobile = 'mobile',
-    received = false,
-    drawn = false,
-    type = '2',
-    prize_msg = '恭喜您获得1.00元微信现金红包, 将通过公众号发放, 请尽快领奖!';
+    var prizeInfo = {
+        '0': {
+            'winId': 7
+        },
+        '1': {
+            'winId': 2,
+            'prize-level': '一等奖',
+        },
+        '2': {
+            'winId': 4,
+            'prize-level': '二等奖',
+        },
+        '3': {
+            'winId': 10,
+            'prize-level': '三等奖',
+        }
+    };
 
-var prizeInfo = {
-    '0': {
-        'winId': 7
-    },
-    '1': {
-        'winId': 2,
-        'prize-level': '一等奖',
-    },
-    '2': {
-        'winId': 4,
-        'prize-level': '二等奖',
-    },
-    '3': {
-        'winId': 10,
-        'prize-level': '三等奖',
-    }
-};
+    $('#start-btn').bind('click', function () {
+        $('#start-btn').remove();
+        $('#message-content').text('<i class="fa fa-spinner fa-spin fa-fw"></i>');
+        if (!navigator.geolocation) {
+            $('#message-content').text('浏览器不支持获取位置, 无法参与抽奖!');
+        }
 
-$('#start-btn').bind('click', function () {
-    $('#start-btn').remove();
-    $('#message button').after('<i class="fa fa-spinner fa-spin fa-fw"></i>');
-    if (!navigator.geolocation) {
-        $('#message button').after('浏览器不支持获取位置, 无法参与抽奖!');
-    }
+        function success(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            $('#message').removeClass('text-center');
+            $('#message button').removeClass('hide');
+            $.ajax({
+                type: 'POST',
+                url: '/lottery/locate',
+                data: {
+                    'token': token,
+                    'id': id,
+                    '_csrf': _csrf,
+                    'longitude': longitude,
+                    'latitude': latitude
+                },
+                ContentType: "application/x-www-form-urlencoded",
+                dataType: "json",
+                success: function (data) {
+                    $('#message i').remove();
+                    if (0 == data['code']) {
+                        type = data['data']['type'];
+                        received = data['data']['received'];
+                        drawn = data['data']['drawn'];
+                        prize_msg = data['data']['msg'];
+                        if (drawn) {
+                            if (received) {
+                                $('#message-content').text('您已兑奖成功, 感谢您的支持!');
+                            }
+                            else {
+                                if ('0' == type) {
+                                    $('#message-content').text('您已抽奖, 很遗憾您没有中奖, 感谢您的支持!')
+                                }
+                                else {
+                                    $('#message-content').html(prize_msg + ', <span class="confirm-btn" style="color: red">点击前往领奖</span>');
+                                    $('.confirm-btn').bind('click', confirmQuery);
+                                }
+                            }
+                        }
+                        else {
+                            $('#message').addClass('hide');
+                            $('.background').removeClass('hide');
+                            $('.background').height($('.background').width() * 0.8);
+                            $('.wrapper .background .prizes li').css({
+                                'line-height': String($('.wrapper .background .prizes li').height()) + 'px',
+                                'font-size': String($('.wrapper .background .prizes li').height() / 4) + 'px'
+                            });
+                            $('.wrapper .background .dashboard a').css({
+                                'line-height': String($('.wrapper .background .dashboard a').height()) + 'px',
+                                'font-size': String($('.wrapper .background .dashboard a').height() / 3) + 'px'
+                            });
+                        }
+                    }
+                    else {
+                        $('#message-content').text(data['msg']);
+                    }
+                },
+                error: function (xml, e) {
+                    $('#message i').remove();
+                    $('#message-content').text('something error');
+                    console.log(e);
+                }
+            });
 
-    function success(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        $('#message').removeClass('text-center');
-        $('#message button').removeClass('hide');
+            console.log(latitude, longitude);
+        }
+
+        function error() {
+            $('#message').removeClass('text-center');
+            $('#message button').removeClass('hide');
+            $('#message i').remove();
+            $('#message-content').text('无法获取您的位置, 无法参与抽奖!');
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
+    });
+
+    $('.confirm-btn').bind('click', confirmQuery);
+
+    function confirmQuery() {
         $.ajax({
             type: 'POST',
-            url: '/lottery/locate',
+            url: '/lottery/confirm',
             data: {
                 'token': token,
-                'id': id,
                 '_csrf': _csrf,
                 'longitude': longitude,
                 'latitude': latitude
             },
-            ContentType: "application/x-www-form-urlencoded",
             dataType: "json",
+            ContentType: "application/x-www-form-urlencoded",
             success: function (data) {
-                $('#message i').remove();
                 if (0 == data['code']) {
-                    type = data['data']['type']
-                    received = data['data']['received']
-                    drawn = data['data']['drawn']
-                    prize_msg = data['data']['msg']
-                    if (drawn) {
-                        if (received) {
-                            $('#message button').after('您已兑奖成功, 感谢您的支持!');
-                        }
-                        else {
-                            if ('0' == type) {
-                                $('#message button').after('您已抽奖, 很遗憾您没有中奖, 感谢您的支持!')
-                            }
-                            else {
-                                $('#message-content').html(prize_msg + ', <span class="confirm-btn" style="color: red">点击前往领奖</span>');
-                                $('.confirm-btn').bind('click', confirmQuery);
-                            }
-                        }
-                    }
-                    else {
-                        $('#message').addClass('hide');
-                        $('.background').removeClass('hide');
-                        $('.background').height($('.background').width() * 0.8);
-                        $('.wrapper .background .prizes li').css({
-                            'line-height': String($('.wrapper .background .prizes li').height()) + 'px',
-                            'font-size': String($('.wrapper .background .prizes li').height() / 4) + 'px'
-                        });
-                        $('.wrapper .background .dashboard a').css({
-                            'line-height': String($('.wrapper .background .dashboard a').height()) + 'px',
-                            'font-size': String($('.wrapper .background .dashboard a').height() / 3) + 'px'
-                        });
-                    }
+                    $('#message-content').text('领奖成功, 请前往微信钱包查看!');
                 }
                 else {
                     $('#message-content').text(data['msg']);
                 }
-            },
-            error: function (xml, e) {
-                $('#message i').remove();
-                $('#message-content').text('something error');
-                console.log(e);
+                $('#prize-info').modal('hide');
+                $('#message').removeClass('hide');
             }
         });
-
-        console.log(latitude, longitude);
+        $('.confirm-btn').unbind('click');
     }
 
-    function error() {
-        $('#message').removeClass('text-center');
-        $('#message button').removeClass('hide');
-        $('#message i').remove();
-        $('#message-content').text('无法获取您的位置, 无法参与抽奖!');
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error);
-});
-
-$('.confirm-btn').bind('click', confirmQuery);
-
-function confirmQuery() {
-    $.ajax({
-        type: 'POST',
-        url: '/lottery/confirm',
-        data: {
-            'token': token,
-            '_csrf': _csrf,
-            'longitude': longitude,
-            'latitude': latitude
-        },
-        dataType: "json",
-        ContentType: "application/x-www-form-urlencoded",
-        success: function (data) {
-            if (0 == data['code']) {
-                $('#message-content').text('领奖成功, 请前往微信钱包查看!');
-            }
-            else {
-                $('#message-content').text(data['msg']);
-            }
-            $('#prize-info').modal('hide');
-            $('#message').removeClass('hide');
-        }
-    });
-    $('.confirm-btn').unbind('click');
-}
-
-$(function () {
     $('#lottery-start').bind('click', function () {
         $('#lottery-start').unbind('click');
         $('.drawbtn').css({
@@ -153,11 +149,11 @@ $(function () {
             'text-decoration': 'none'
         });
         $('.prizes').lottery({
-            winId: Math.ceil(prizeInfo[type]['winId'] - 1), //中奖序号
-            round: 3, //跑的基础轮次
-            time: 5, //动画时间
-            activeClass: 'active', //中奖高亮样式
-            effect: 'Cubic', //动画曲线
+            winId: Math.ceil(prizeInfo[type]['winId'] - 1),
+            round: 3,
+            time: 5,
+            activeClass: 'active',
+            effect: 'Cubic',
             finish: function () {
                 if ('0' == type) {
                     $('.modal-body p').text('很遗憾你没有中奖, 感谢您的支持!');
